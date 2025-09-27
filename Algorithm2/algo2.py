bcfc531ac11e845189b52abc76e7d2e538d4b028
Natalia Garcia <natgarcia@csu.fullerton.edu>
2025-09-27 19:09:03 +0000

    add .txt & code

--- Algorithm2/algo2.py
+++ Algorithm2/algo2.py
@@ -35,15 +35,65 @@ def FindTimes(schedule, person1, person2)
 
 """
 
-import datetime from datetime
+from datetime import datetime
 
+# opens file to get sample input
+with open("sample_input.txt", "r") as file:
+    data = file.read()  # reads data
+exec(data) # executes & defines variables
+
+p1_sched = person1_Schedule  # list of times not available 
+p1_avail = person1_DailyAct  # list of times available
+p2_sched = person2_Schedule  # list of times not available 
+p2_avail = person2_DailyAct  # list of times available
+duration = duration_of_meeting
 
 
 """Method converting time string for comparison"""
 def conTime(time):
     time_format = "%H:%M"
-    initial = time
+    conv_time = datetime.strptime(time, time_format)
 
-    conv_time = datetime.strptime(initial, time_format)
+
+"""Function that returns a persons availability in time intervals given their schedule and availability"""
+def AvailabilityIntervals(sched, avail):
+    avail_int = avail
+    current = avail[0]
+    last = avail[1]
+    last_time = sched[-1][-1]
+    schedule = []
+    gap_times = []
+
+    conTime(current)
+    conTime(last)
+    
+    for start, end in sched:
+        # print statements to check for errors
+        print("Current:", current)
+        print("Start:", start + ", " + end)
+        print("Schedule:", sched)
+        conTime(start)
+        conTime(end)
+
+        if start > current:
+            gap_times.append([current, start])
+        elif current > start and current > end:
+            end_time = sched[1][0]
+            gap_times.append([current, end_time])
+            # current = end_time
+        else:
+            current = end 
+        print("Gaps:", gap_times)
+
+        if end < current:
+            current = avail[0]
+        current = end
+        print("Current 2:", current,"\n") 
+
+
+new_sched = AvailabilityIntervals(p1_sched, p1_avail)
+print(new_sched,"\n")
+new_sched2 = AvailabilityIntervals(p2_sched, p2_avail)
+print(new_sched2)
 
 
